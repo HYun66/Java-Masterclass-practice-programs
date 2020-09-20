745445e1a334f308d13cc5bf579dffa6fe0f2756
HYun66 <niuhy0728@gmail.com>
2020-09-20 18:43:20 +1000

    Add an adventure game using HashMap

---
+++ HashMapAdventure/src/com/huiyun/Location.java
@@ -0,0 +1,33 @@
+package com.huiyun;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+        exits.put("Q", 0);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
