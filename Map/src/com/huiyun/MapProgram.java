b59c8e9a2f2483c1407a414b616fd2ea78f8b6d5
HYun66 <niuhy0728@gmail.com>
2020-09-20 18:27:19 +1000

    Add example of HashMap

---
+++ Map/src/com/huiyun/MapProgram.java
@@ -0,0 +1,48 @@
+package com.huiyun;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        if(languages.containsKey("Java")) {
+            System.out.println("Java already exists");
+        } else {
+            languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
+        }
+
+        languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        languages.put("Algol", "a programming algorithmic language");
+        System.out.println(languages.put("BASIC", "Beginners All Purposes Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp", "Therein lies madness"));
+
+        if(languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java", "this course is about Java");
+        }
+
+        System.out.println("================================================");
+
+        for(String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
+
+        for(String value: languages.values()) {
+            System.out.println(value);
+        }
+
+        languages.remove("Lisp");
+        languages.replace("Algol", "a new language");
+
+        languages.forEach((key, value) -> {
+            System.out.println("key: " + key + "; " + "value: " + value);
+        });
+
+
+    }
+
+}
