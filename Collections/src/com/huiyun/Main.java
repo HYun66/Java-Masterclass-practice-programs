1e0af5f5177c6775f6497cc5cbbf73570282558a
HYun66 <niuhy0728@gmail.com>
2020-09-20 14:50:12 +1000

    Add example of Comparator

--- Collections/src/com/huiyun/Main.java
+++ Collections/src/com/huiyun/Main.java
@@ -1,21 +1,41 @@
 package com.huiyun;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Theatre theatre = new Theatre("Star", 10, 12);
-	    theatre.getSeats();
 
-	    if (theatre.reserveSeat("E07")) {
+        if (theatre.reserveSeat("A02")) {
             System.out.println("Please pay");
         } else {
-            System.out.println("Sorry, seat is taken");
+            System.out.println("Seat already reserved");
         }
 
-        if (theatre.reserveSeat("A07")) {
+        if (theatre.reserveSeat("E06")) {
             System.out.println("Please pay");
         } else {
-            System.out.println("Sorry, seat is taken");
+            System.out.println("Seat already reserved");
         }
+
+        List<Theatre.Seat> reverseSeat = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeat);
+        printList(reverseSeat);
+
+        List<Theatre.Seat> priceSeat = new ArrayList<>(theatre.getSeats());
+        priceSeat.add(theatre.new Seat("B00", 13.00));
+        priceSeat.add(theatre.new Seat("A00", 13.00));
+        Collections.sort(priceSeat, Theatre.PRICE_ORDER);
+        printList(priceSeat);
     }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber() + " $" + seat.getPrice());
+        }
+        System.out.println();
+        System.out.println("===========================================");
+    }
+
+
 }
